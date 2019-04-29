d0343a1054d6479feb7f404017164152adbb6f4c
Olli Oittinen <ollioi@metropolia.fi>
2019-04-29 14:33:39 +0300

    3.3 complete

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -41,6 +41,16 @@ app.get('/info', (req, res) => {
     res.send(message)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 
 app.listen(PORT, () => {
